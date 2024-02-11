fad25c4c019946f37c70adc190a5a9fd63fbb1fd
zerg959 <zerg959@yandex.ru>
2024-02-12 00:42:10 +0300

    refactoring of even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,3 +2,7 @@
 
 import { evenTask, evenGame } from "../z/even.js"
 import { startMessage, randomInt, myHeroName, greets, answerInput } from "../dialogues/functions.js";
+
+
+const attempts = 3;
+let counter = 0;
