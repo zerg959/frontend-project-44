2250e97ca4c7c9c34f5e85a6d900422db1b7687f
zerg959 <zerg959@yandex.ru>
2024-02-12 01:51:24 +0300

    refactoring calc-data.js

--- src/calc-data.js
+++ src/calc-data.js
@@ -2,10 +2,6 @@ import { randomInt } from "./functions.js";
 
 const calcTask = 'What is the result of the expression?';
 
-// const evenCheck = (num1, num2) => {
-//   return num % 2 === 0;
-// };
-
 const calcGame = () => {
   let num1 = randomInt(1,11);
   let num2 = randomInt(1,11);
