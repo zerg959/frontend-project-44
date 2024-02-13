fb3115e3d6b6a6fe0172f8e98f627a229ad8041b
zerg959 <zerg959@yandex.ru>
2024-02-14 02:38:20 +0300

    Linter: progression-data

--- src/games/progression-data.js
+++ src/games/progression-data.js
@@ -5,11 +5,11 @@ const progressionTask = 'What number is missing in the progression?';
 
 const progressionGame = () => {
   const progressionStep = randomInt(1, 15);
-  const progressionLen = randomInt(5, 15)
-  const progressionStart = randomInt(1, 50)
-  let progression = range(progressionStart, 1001, progressionStep);
-  let newProgr = progression.slice(0, progressionLen);
-  let estimatedAnsw = newProgr.splice(randomInt(1, progressionLen - 1),1,'..');
+  const progressionLen = randomInt(5, 15);
+  const progressionStart = randomInt(1, 50);
+  const progression = range(progressionStart, 1001, progressionStep);
+  const newProgr = progression.slice(0, progressionLen);
+  const estimatedAnsw = newProgr.splice(randomInt(1, progressionLen - 1), 1, '..');
   const expression = newProgr.join(' ');
   return [expression, String(estimatedAnsw)];
 };
