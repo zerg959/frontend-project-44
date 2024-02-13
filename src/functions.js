3adc75846099f723244d8fb95766a04755547335
zerg959 <zerg959@yandex.ru>
2024-02-14 01:20:52 +0300

    Error: linter mistakes in functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,24 +1,18 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-let myHeroName = '';
-const randomInt = (max, min) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+const myHeroName = '';
+const randomInt = (max, min) => Math.floor(Math.random() * (max - min)) + min;
 
 const startMessage = (message) => message;
 const answerInput = () => {
-  let heroAnswer = readlineSync.question('Your answer: ');
+  const heroAnswer = readlineSync.question('Your answer: ');
   return heroAnswer;
-  };
+};
 
 const greets = () => {
   console.log('Welcome to the Brain Games!');
   myHeroName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${myHeroName}!`);
 };
-
-// const congrats = (heroName) => {
-//   console.log(`Congratulations, ${heroName}!`);
-// }; 
 
 export { randomInt, startMessage, greets, answerInput, myHeroName };
