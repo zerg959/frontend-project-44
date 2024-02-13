b405877d448544a60f4221fbdb3c87d43ed1e52e
zerg959 <zerg959@yandex.ru>
2024-02-14 01:09:58 +0300

    Error: linter mistakes in prime-data.js

--- src/games/prime-data.js
+++ src/games/prime-data.js
@@ -1,20 +1,20 @@
-import { randomInt } from '../functions.js'
 import range from 'lodash.range';
+import { randomInt } from '../functions.js';
 
 const primeTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const primeCheck = (num) => {
   let primeCounter = 0;
-  for (let i in range(1, num + 1)) {
-  if (num % (Number(i)+1) === 0) {
-      primeCounter += 1
-    } 
+  for (const i in range(1, num + 1)) {
+    if (num % (Number(i) + 1) === 0) {
+      primeCounter += 1;
+    }
   }
   return primeCounter > 2;
 };
 
 const primeGame = () => {
-  let questIsPrime = randomInt(1,101);
+  const questIsPrime = randomInt(1,101);
   let answerIsPrime;
   if (primeCheck(questIsPrime)) {
     answerIsPrime = 'no';
@@ -22,6 +22,6 @@ const primeGame = () => {
     answerIsPrime = 'yes';
   }
   return [questIsPrime, answerIsPrime];
-}
+};
 
 export { primeGame, primeTask };
