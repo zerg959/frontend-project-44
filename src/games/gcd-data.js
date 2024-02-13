c3081fa12f05fb23a4e263a861a8185a04c86efc
zerg959 <zerg959@yandex.ru>
2024-02-14 01:14:06 +0300

    Error: linter mistakes in gcd-data.js

--- src/games/gcd-data.js
+++ src/games/gcd-data.js
@@ -1,16 +1,16 @@
-import { randomInt } from "../functions.js";
+import { randomInt } from '../functions.js';
 
-const maxnum = 50;
-const minnum = 1;
+const maxNum = 50;
+const minNum = 1;
 
 const gcdTask = 'Find the greatest common divisor of given numbers.';
 
 const gcdGame = () => {
-  let num1 = randomInt(maxnum, minnum);
-  let num2 = randomInt(maxnum, minnum);
+  const num1 = randomInt(maxNum, minNum);
+  const num2 = randomInt(maxNum, minNum);
   const divisors1 = [];
   const divisors2 = [];
-  const gcdArr =[];
+  const gcdArr = [];
   for (let i = 1; i <= num1; i += 1) {
     if (num1 % i === 0) {
       divisors1.push(i);
@@ -19,14 +19,14 @@ const gcdGame = () => {
   for (let k = 1; k <= num2; k += 1) {
     if (num2 % k === 0) {
       divisors2.push(k);
-      }
-    }
-  for (let d of divisors1) {
-    if (divisors2.includes(d)) {
-      gcdArr.push(d);
     }
   }
-  let gcd = Math.max(...gcdArr);
+  for (const value of divisors1) {
+    if (divisors2.includes(value)) {
+      gcdArr.push(value);
+    }
+  }
+  const gcd = Math.max(...gcdArr);
   return [`${num1} ${num2}`, String(gcd)];
 };
 
