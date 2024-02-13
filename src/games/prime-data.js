e37d6f7567a983cd077ade0136dc0d4cc842fb00
zerg959 <zerg959@yandex.ru>
2024-02-14 02:41:44 +0300

    Linter: prime-data

--- src/games/prime-data.js
+++ src/games/prime-data.js
@@ -1,12 +1,12 @@
 import range from 'lodash.range';
-import { randomInt } from '../functions.js'
+import { randomInt } from '../functions.js';
 
 const primeTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const primeCheck = (num) => {
   let primeCounter = 0;
-  for (let i in range(1, num + 1)) {
-  if (num % (Number(i)+1) === 0) {
+  for (const i in range(1, num + 1)) {
+    if (num % (Number(i) + 1) === 0) {
       primeCounter += 1;
     }
   }
@@ -14,7 +14,7 @@ const primeCheck = (num) => {
 };
 
 const primeGame = () => {
-  let questIsPrime = randomInt(1, 101);
+  const questIsPrime = randomInt(1, 101);
   let answerIsPrime;
   if (primeCheck(questIsPrime)) {
     answerIsPrime = 'no';
