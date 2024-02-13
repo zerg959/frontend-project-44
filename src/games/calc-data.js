d1c5bde1f86f1f8d9b5f465a3a9c3a6ff80b1594
zerg959 <zerg959@yandex.ru>
2024-02-14 01:18:06 +0300

    Error: linter mistakes in calc-data.js

--- src/games/calc-data.js
+++ src/games/calc-data.js
@@ -1,13 +1,13 @@
-import { randomInt } from "../functions.js";
+import { randomInt } from '../functions.js';
 
 const calcTask = 'What is the result of the expression?';
 
 const calcGame = () => {
-  let num1 = randomInt(1,11);
-  let num2 = randomInt(1,11);
+  const num1 = randomInt(1, 11);
+  const num2 = randomInt(1, 11);
   const operators = ['+', '*'];
-  let operator = operators[randomInt(0,operators.length)];
-  let questExpression = `${num1} ${operator} ${num2}`;
+  const operator = operators[randomInt(0, operators.length)];
+  const questExpression = `${num1} ${operator} ${num2}`;
   return [questExpression, String(eval(questExpression))];
 };
 
