518f39ffecac39b4babd7893505b1fc9c6d2d26a
zerg959 <zerg959@yandex.ru>
2024-02-14 14:47:43 +0300

    Refactoring: eval to switc (calc-data)

--- src/games/calc-data.js
+++ src/games/calc-data.js
@@ -5,10 +5,19 @@ const calcTask = 'What is the result of the expression?';
 const calcGame = () => {
   const num1 = randomInt(1, 11);
   const num2 = randomInt(1, 11);
+  let answerCalc;
   const operators = ['+', '*'];
   const operator = operators[randomInt(0, operators.length)];
+  switch (operator){
+    case '+':
+      answerCalc = num1 + num2;
+      break;
+    case '*':
+      answerCalc = num1 * num2;
+      break;
+  }
   const questExpression = `${num1} ${operator} ${num2}`;
-  return [questExpression, String(eval(questExpression))];
+  return [questExpression, String(answerCalc)];
 };
 
 export { calcTask, calcGame };
