65a0c1dbbcf802a7651da59a8b73d680d1f56fcb
zerg959 <zerg959@yandex.ru>
2024-02-14 13:44:12 +0300

    Refactoring: greets

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 
-let myHeroName = '';
 const randomInt = (max, min) => Math.floor(Math.random() * (max - min)) + min;
 
 const startMessage = (message) => message;
@@ -9,16 +8,8 @@ const answerInput = () => {
   return heroAnswer;
 };
 
-const greets = () => {
-  console.log('Welcome to the Brain Games!');
-  myHeroName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${myHeroName}!`);
-};
-
 export {
   randomInt,
   startMessage,
-  greets,
-  answerInput,
-  myHeroName,
+  answerInput
 };
