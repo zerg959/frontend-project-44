40bd0b454ac03681daed1ec72c9b92cc113cd54b
zerg959 <zerg959@yandex.ru>
2024-02-14 13:13:09 +0300

    Linter: default import in cli.js

--- cli/cli.js
+++ cli/cli.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const cli = () => {
+export default function () {
   console.log('Welcome to the Brain Games!');
   const myName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${myName}!`);
   return myName;
 };
-
-export { cli };
